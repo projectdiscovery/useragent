85ac381d91a92be07d2bf26938f7ff95d65d754d
Dogan Can Bakir <65292895+dogancanbakir@users.noreply.github.com>
2024-01-30 17:17:07 +0300

    use whatismybrowser (#108)
    * use whatismybrowser
    
    * update .gitignore
    
    * fix lint, no need
    
    * minor
    
    * fail if api key is not available
    
    * misc workflow update
    
    ---------
    
    Co-authored-by: sandeep <8293321+ehsandeep@users.noreply.github.com>

--- cmd/update-useragent/main.go
+++ cmd/update-useragent/main.go
@@ -1,41 +1,33 @@
 package main
 
 import (
-	"bytes"
-	"compress/gzip"
 	"crypto/tls"
 	"encoding/json"
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
 
-	"github.com/antchfx/xmlquery"
 	"github.com/projectdiscovery/useragent"
 	sliceutil "github.com/projectdiscovery/utils/slice"
-	stringsutil "github.com/projectdiscovery/utils/strings"
 )
 
 var (
 	userAgentData = flag.String("user-agents", "../../useragent_data.json", "File to write user agents to")
+	browsers      = []string{"chrome", "edge", "safari", "firefox", "firefox-esr"}
 )
 
 func main() {
-	// load user agents from
 	flag.Parse()
 
-	uas, err := parseSources()
+	userAgents := getUserAgents()
+	data, err := json.Marshal(userAgents)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	data, err := json.Marshal(uas)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// dump to output file
 	err = os.WriteFile(*userAgentData, data, 0644)
 	if err != nil {
 		log.Fatal(err)
@@ -43,8 +35,83 @@ func main() {
 
 }
 
-// getHttpClient with common options
-func getHttpClient() *http.Client {
+func getUserAgents() []*useragent.UserAgent {
+	var userAgents []*useragent.UserAgent
+
+	httpClient := buildHttpClient()
+	for _, browser := range browsers {
+		whatismybrowserApiUrl := fmt.Sprintf(`https://api.whatismybrowser.com/api/v2/user_agent_database_search?order_by=times_seen%%20desc&limit=300&offset=500&software_name=%s`, browser)
+		req, err := http.NewRequest("GET", whatismybrowserApiUrl, nil)
+		if err != nil {
+			continue
+		}
+		apiKey := os.Getenv("WHATISMYBROWSER_KEY")
+		if apiKey == "" {
+			log.Fatal("API key is empty. Please set the WHATISMYBROWSER_KEY environment variable.")
+		}
+		req.Header.Add("X-API-KEY", apiKey)
+
+		resp, err := httpClient.Do(req)
+		if err != nil {
+			continue
+		}
+
+		data, err := io.ReadAll(resp.Body)
+		if err != nil {
+			continue
+		}
+
+		var whatismybrowserResponse WhatismybrowserResponse
+		err = json.Unmarshal(data, &whatismybrowserResponse)
+		if err != nil {
+			continue
+		}
+		for _, userAgent := range whatismybrowserResponse.SearchResults.UserAgents {
+			tags := buildTags(userAgent)
+			userAgent := &useragent.UserAgent{
+				Raw:  userAgent.UserAgent,
+				Tags: sliceutil.Dedupe(sliceutil.PruneEmptyStrings(tags)),
+			}
+			userAgents = append(userAgents, userAgent)
+		}
+	}
+	return userAgents
+}
+
+func buildTags(userAgent UserAgents) []string {
+	var tags []string
+	tags = append(tags, userAgent.Parse.SimpleSoftwareString)
+	tags = append(tags, fmt.Sprintf("%v", userAgent.Parse.SimpleSubDescriptionString))
+	tags = append(tags, fmt.Sprintf("%v", userAgent.Parse.SimpleOperatingPlatformString))
+	tags = append(tags, userAgent.Parse.Software)
+	tags = append(tags, userAgent.Parse.SoftwareName)
+	tags = append(tags, userAgent.Parse.SoftwareNameCode)
+	tags = append(tags, userAgent.Parse.SoftwareVersion)
+	tags = append(tags, userAgent.Parse.SoftwareVersionFull)
+	tags = append(tags, userAgent.Parse.OperatingSystem)
+	tags = append(tags, userAgent.Parse.OperatingSystemName)
+	tags = append(tags, userAgent.Parse.OperatingSystemNameCode)
+	tags = append(tags, userAgent.Parse.OperatingSystemVersion)
+	tags = append(tags, userAgent.Parse.OperatingSystemVersionFull)
+	tags = append(tags, fmt.Sprintf("%v", userAgent.Parse.OperatingPlatform))
+	tags = append(tags, fmt.Sprintf("%v", userAgent.Parse.OperatingPlatformCode))
+	tags = append(tags, fmt.Sprintf("%v", userAgent.Parse.OperatingPlatformVendorName))
+	tags = append(tags, userAgent.Parse.SoftwareType)
+	tags = append(tags, userAgent.Parse.SoftwareSubType)
+	tags = append(tags, userAgent.Parse.HardwareType)
+	tags = append(tags, fmt.Sprintf("%v", userAgent.Parse.HardwareSubType))
+	tags = append(tags, fmt.Sprintf("%v", userAgent.Parse.HardwareSubSubType))
+	tags = append(tags, userAgent.Parse.SoftwareTypeSpecific)
+	tags = append(tags, userAgent.Parse.HardwareTypeSpecific)
+	tags = append(tags, userAgent.Parse.LayoutEngineName)
+	for _, version := range userAgent.Parse.LayoutEngineVersion {
+		tags = append(tags, fmt.Sprintf("%v", version))
+	}
+	return tags
+}
+
+// buildHttpClient with common options
+func buildHttpClient() *http.Client {
 	transport := http.Transport{
 		Proxy: http.ProxyFromEnvironment,
 		TLSClientConfig: &tls.Config{
@@ -55,136 +122,72 @@ func getHttpClient() *http.Client {
 	return &http.Client{Transport: &transport}
 }
 
-// parseSources and retrieve a list of user agents with metadata
-func parseSources() ([]*useragent.UserAgent, error) {
-	uas1, err := parseTechPattern()
-	if err != nil {
-		return nil, err
-	}
-	uas2, err := parseGhRepo()
-	if err != nil {
-		return nil, err
-	}
-
-	return append(uas1, uas2...), nil
+type UserAgentMetaData struct {
+	ID          int    `json:"id"`
+	TimesSeen   int    `json:"times_seen"`
+	FirstSeenAt string `json:"first_seen_at"`
+	LastSeenAt  string `json:"last_seen_at"`
 }
 
-const UrlTechPattern = "https://techpatterns.com/downloads/firefox/useragentswitcher.xml"
-
-// Parses source at "https://techpatterns.com/downloads/firefox/useragentswitcher.xml"
-func parseTechPattern() ([]*useragent.UserAgent, error) {
-	httpClient := getHttpClient()
-
-	resp, err := httpClient.Get(UrlTechPattern)
-	if err != nil {
-		return nil, err
-	}
-
-	data, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	doc, err := xmlquery.Parse(bytes.NewReader(data))
-	if err != nil {
-		return nil, err
-	}
-
-	var userAgents []*useragent.UserAgent
-	folders := doc.SelectElements("//folder")
-	for _, folder := range folders {
-		folderDescription := folder.SelectAttr("description")
-		if stringsutil.EqualFoldAny(folderDescription, "UA List :: About") {
-			continue
-		}
-		folderAgents := folder.SelectElements("//useragent")
-		for _, folderAgent := range folderAgents {
-			var tags []string
-			tags = append(tags, folder.SelectAttr("description"))
-			tags = append(tags, folderAgent.SelectAttr("description"))
-			tags = append(tags, folderAgent.SelectAttr("appcodename"))
-			tags = append(tags, folderAgent.SelectAttr("appname"))
-			tags = append(tags, folderAgent.SelectAttr("appversion"))
-			tags = append(tags, folderAgent.SelectAttr("platform"))
-			tags = append(tags, folderAgent.SelectAttr("vendor"))
-			tags = append(tags, folderAgent.SelectAttr("vendorsub"))
-
-			// add parent folder as tag
-			userAgent := &useragent.UserAgent{
-				Raw:  folderAgent.SelectAttr("useragent"),
-				Tags: sliceutil.Dedupe(sliceutil.PruneEmptyStrings(tags)),
-			}
-			userAgents = append(userAgents, userAgent)
-		}
-	}
-
-	return userAgents, nil
+type Parse struct {
+	SimpleSoftwareString          string      `json:"simple_software_string"`
+	SimpleSubDescriptionString    interface{} `json:"simple_sub_description_string"`
+	SimpleOperatingPlatformString interface{} `json:"simple_operating_platform_string"`
+	Software                      string      `json:"software"`
+	SoftwareName                  string      `json:"software_name"`
+	SoftwareNameCode              string      `json:"software_name_code"`
+	SoftwareVersion               string      `json:"software_version"`
+	SoftwareVersionFull           string      `json:"software_version_full"`
+	OperatingSystem               string      `json:"operating_system"`
+	OperatingSystemName           string      `json:"operating_system_name"`
+	OperatingSystemNameCode       string      `json:"operating_system_name_code"`
+	OperatingSystemVersion        string      `json:"operating_system_version"`
+	OperatingSystemVersionFull    string      `json:"operating_system_version_full"`
+	OperatingPlatform             interface{} `json:"operating_platform"`
+	OperatingPlatformCode         interface{} `json:"operating_platform_code"`
+	OperatingPlatformVendorName   interface{} `json:"operating_platform_vendor_name"`
+	SoftwareType                  string      `json:"software_type"`
+	SoftwareSubType               string      `json:"software_sub_type"`
+	HardwareType                  string      `json:"hardware_type"`
+	HardwareSubType               interface{} `json:"hardware_sub_type"`
+	HardwareSubSubType            interface{} `json:"hardware_sub_sub_type"`
+	SoftwareTypeSpecific          string      `json:"software_type_specific"`
+	HardwareTypeSpecific          string      `json:"hardware_type_specific"`
+	LayoutEngineName              string      `json:"layout_engine_name"`
+	ExtraInfo                     struct {
+	} `json:"extra_info"`
+	LayoutEngineVersion []interface{} `json:"layout_engine_version"`
 }
 
-const UrlGhRepository = "https://github.com/intoli/user-agents/raw/main/src/user-agents.json.gz"
-
-// parseGhRepo at UrlGhRepository
-func parseGhRepo() ([]*useragent.UserAgent, error) {
-	httpClient := getHttpClient()
-
-	resp, err := httpClient.Get(UrlGhRepository)
-	if err != nil {
-		return nil, err
-	}
-
-	r, err := gzip.NewReader(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-	defer r.Close()
-
-	data, err := io.ReadAll(r)
-	if err != nil {
-		return nil, err
-	}
-
-	var uas []intoliUserAgent
-
-	if err := json.Unmarshal(data, &uas); err != nil {
-		return nil, err
-	}
-
-	var userAgents []*useragent.UserAgent
-	for _, ua := range uas {
-		var tags []string
-		tags = append(tags, ua.AppName)
-		tags = append(tags, ua.DeviceCategory)
-		tags = append(tags, ua.Platform)
-		tags = append(tags, ua.Vendor)
-
-		// add parent folder as tag
-		userAgent := &useragent.UserAgent{
-			Raw:  ua.UserAgent,
-			Tags: sliceutil.Dedupe(sliceutil.PruneEmptyStrings(tags)),
-		}
-		userAgents = append(userAgents, userAgent)
-	}
-
-	return userAgents, nil
+type UserAgents struct {
+	UserAgent         string            `json:"user_agent"`
+	UserAgentMetaData UserAgentMetaData `json:"user_agent_meta_data"`
+	Parse             Parse             `json:"parse"`
 }
 
-type intoliConnection struct {
-	DownLink      float64 `json:"downlink"`
-	EffectiveType string  `json:"effectiveType"`
-	RTT           int     `json:"rtt"`
+type SearchParameters struct {
+	SoftwareName string `json:"software_name"`
+	HardwareType string `json:"hardware_type"`
 }
 
-type intoliUserAgent struct {
-	AppName        string           `json:"appName"`
-	Connection     intoliConnection `json:"connection"`
-	Platform       string           `json:"platform"`
-	PluginsLength  int              `json:"pluginsLength"`
-	Vendor         string           `json:"vendor"`
-	UserAgent      string           `json:"userAgent"`
-	ViewportHeight int              `json:"viewportHeight"`
-	ViewportWidth  int              `json:"viewportWidth"`
-	DeviceCategory string           `json:"deviceCategory"`
-	ScreenHeight   int              `json:"screenHeight"`
-	ScreenWidth    int              `json:"screenWidth"`
-	Weight         float64          `json:"weight"`
+type SearchMetaData struct {
+	NumOfResultsReturned   int              `json:"num_of_results_returned"`
+	SearchTookMilliseconds int              `json:"search_took_milliseconds"`
+	SearchParameters       SearchParameters `json:"search_parameters"`
+}
+
+type Result struct {
+	Code        string `json:"code"`
+	MessageCode string `json:"message_code"`
+	Message     string `json:"message"`
+}
+
+type SearchResults struct {
+	UserAgents     []UserAgents   `json:"user_agents"`
+	SearchMetaData SearchMetaData `json:"search_meta_data"`
+}
+
+type WhatismybrowserResponse struct {
+	SearchResults SearchResults `json:"search_results"`
+	Result        Result        `json:"result"`
 }
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/projectdiscovery/useragent
 
-go 1.18
+go 1.21
 
 require (
 	github.com/antchfx/xmlquery v1.3.17
@@ -11,12 +11,10 @@ require (
 )
 
 require (
-	github.com/antchfx/xpath v1.2.4 // indirect
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
 	github.com/aymerick/douceur v0.2.0 // indirect
 	github.com/cnf/structhash v0.0.0-20201127153200-e1b16c1ebc08 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/gorilla/css v1.0.0 // indirect
 	github.com/microcosm-cc/bluemonday v1.0.25 // indirect
 	github.com/miekg/dns v1.1.56 // indirect
@@ -28,7 +26,6 @@ require (
 	golang.org/x/mod v0.12.0 // indirect
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/tools v0.13.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
